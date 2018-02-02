f1095088bde9faf9a736f501521b39c52d3cf60f
Jyckle <jycklepasquesi@gmail.com>
2018-02-02 16:50:56 -0600

    Before practice

--- HW1/src/Liebniz.java
+++ HW1/src/Liebniz.java
@@ -1,6 +1,18 @@
 
 public class Liebniz {
 	public static void main(String[] args) {
-	
+		
+		int acc = 50;
+		double piApprox = 0.0;
+		for (int i = 0; i< acc; i++)
+		{
+			double next = ((-1)^i) * 1/(2.0*i + 1.0);
+			piApprox += next;
+			System.out.println(piApprox);
+		}
+		
+		piApprox = piApprox * 4;
+		
+		System.out.println("Pi is roughly equal to: " + piApprox + " with " + acc + " iterations");
 	}
 }
